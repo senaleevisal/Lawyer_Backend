a10e5cac27e0f862dd036e55c4a357537343f3b5
Senalee Visal <m.m.senaleevisal@gmail.com>
2024-08-01 09:46:23 +0530

    hot fix

--- src/main/java/edu/ait/lawyer/LawyerApplication.java
+++ src/main/java/edu/ait/lawyer/LawyerApplication.java
@@ -1,5 +1,6 @@
 package edu.ait.lawyer;
 
+
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
